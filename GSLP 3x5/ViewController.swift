9135798c2e7743bcbff01fd8dd6c683129c21671
Nazarzbs <famina1998@gmail.com>
2023-07-24 02:52:50 +0300

    Initial Commit

---
+++ GSLP 3x5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GSLP 3x5
+//
+//  Created by Nazar on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
